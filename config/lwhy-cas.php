19c0488fd8596b29e1682d7f67ce1742be2a066b
hsk9044 <qyr9044@hotmail.com>
2021-04-06 17:53:30 +0800

    create

---
+++ config/lwhy-cas.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2021/4/6
+ * Time: 17:38
+ */
---
+++ src/LwhyCasClientServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hsk9044\LwhyCasClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class LwhyCasClientServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
