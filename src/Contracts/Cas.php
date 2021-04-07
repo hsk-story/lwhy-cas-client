853e60cc0a9ef59261453c391c2c42a1e43ca19a
hsk <qyr9044@hotmail.com>
2021-04-07 18:18:59 +0800

    提交

--- src/Contracts/Cas.php
+++ src/Contracts/Cas.php
@@ -4,7 +4,9 @@
 namespace Hsk9044\LwhyCasClient\Contracts;
 
 
+use Hsk9044\LwhyCasClient\Cache\UserCache;
 use Hsk9044\LwhyCasClient\Traits\CurlClient;
+use Illuminate\Support\Str;
 
 class Cas
 {
@@ -18,11 +20,32 @@ class Cas
 
     use CurlClient;
 
-    public function authCheck($ticket, $id) {
-        return $this->post('auth', [
+
+    /**
+     * 通过ticket验证用户
+     * @param $ticket
+     * @param $userId
+     * @return mixed
+     */
+    public function authCheck($ticket, $userId) {
+        $result = $this->post('auth', [
             'ticket' => $ticket,
-            'id' => $id,
+            'id' => $userId,
             'return_menu' => 'y',
+            'return_permission' => 'y'
         ]);
+
+        $token = Str::random(32);
+        $casUser = new CasUser();
+        $casUser->put('user_id', $userId);
+        $casUser->put('ticket', $ticket);
+        $casUser->put('permissions', $result['return_permission']);
+        $casUser->put('roles', $result['roles']);
+        $casUser->put('auth_interval');
+        $casUser->put('token', $token);
+
+        $casUser->save($token, $result['info']['auth_interval'] ?? 300);
+
+        return $result;
     }
 }
--- src/Contracts/CasUser.php
+++ src/Contracts/CasUser.php
@@ -18,15 +18,44 @@ use Illuminate\Contracts\Auth\Access\Gate;
 
 class CasUser implements AuthorizableContract{
     use Authorizable;
-//    use CasAuthenticatable;
 
-    protected $attr;
+    protected $attributes = [];
 
-    public function __construct(array $attributes) {
-        $this->attr = $attributes;
+
+    public function __construct() {
+/*        $this->ticket = $ticket;
+        $this->userId = $userId;
+        $this->permissions = $permissions;
+        $this->roles = $roles;*/
     }
 
+
+    public function put($key, $value = '') {
+        if(is_array($key))
+            $this->attributes = array_merge($this->attributes, $key);
+        else
+            $this->attributes[$key] = $value;
+    }
+
+
+
+    //TODO 契约检测权限方法
     public function checkCasPermission($permission, $guardName = null) {
         dd($permission);
+    }
+
+    //保存到缓存
+    public function save($key, $authInterval) {
+        //每个key的缓存保持24小时, 如果key丢失, 则直接跳到登录页
+        //每隔authInterval秒的时间需要重新去cas服务器校验一下ticket, 如果ticket失效则挑到登录页
+        //如果校验成功则更新上次校验秒的缓存
+
+    }
+
+
+    private function camelize($uncamelized_words,$separator='_')
+    {
+        $uncamelized_words = $separator. str_replace($separator, " ", strtolower($uncamelized_words));
+        return ltrim(str_replace(" ", "", ucwords($uncamelized_words)), $separator );
     }
 }
